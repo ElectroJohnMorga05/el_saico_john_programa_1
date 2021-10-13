7736fddcee97deead716d6a9848e6721e70dd858
JohnMorgan6 <90010603+ElectroJohnMorga05@users.noreply.github.com>
2021-10-13 15:45:04 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout <<" John \n " ;// Se pone el nombre 
+  cout << "";// No tiene segundo nombre 
+  cout <<  " Morgan \n " ;// Se pone el primer  apellido 
+  cout << " Diaz \n " ;// se pone el segundo apellido
+  cout << " 10-2 B \n" ;// se pone la seccion
+}
